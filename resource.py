29d2dc07874ada626339ff2545fd3dc84d9a39c9
Mudika Dhyani <147969567+Mudika-Dhyani@users.noreply.github.com>
2024-11-29 17:20:04 +0530

    resource.py

---
+++ resource.py
@@ -0,0 +1,15 @@
+class Resources:
+    """
+    Implements addResource to add the resources.
+    """
+
+    def __init__(self):
+        # Initialize resources
+        self.resources = {}
+
+    def addResource(self, resource, parents=[]):
+        """
+        Add a resource or append resource.
+        """
+        self.resources.setdefault(resource, set())
+        self.resources[resource].update(parents)
